1c255ad0c1b8f3f81e307e79b79131b118c37697
Alessandro Solbiati <lessandro@fb.com>
2022-04-16 13:59:27 +0100

    [hardhat] deploy.js

---
+++ scripts/deploy.js
@@ -0,0 +1,13 @@
+async function main(){
+	const HelloWorld = await ethers.getContractFactory("HelloWorld");
+
+	const hello_world = await HelloWorld.deploy("Hello World");
+	console.log(`deployed at ${ hello_world.address }`);
+}
+
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+	console.log(error);
+	process.exit(1);
+});
