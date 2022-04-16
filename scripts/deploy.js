e73eafca60cf690237bd80072aba518e5a55492f
Alessandro Solbiati <lessandro@fb.com>
2022-04-16 14:24:33 +0100

    [contract] running HelloWorld contract from testnet

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,7 @@
 async function main(){
 	const HelloWorld = await ethers.getContractFactory("HelloWorld");
 
+	// this is the constructor of the smart contract
 	const hello_world = await HelloWorld.deploy("Hello World");
 	console.log(`deployed at ${ hello_world.address }`);
 }
--- scripts/interact.js
+++ scripts/interact.js
@@ -4,3 +4,27 @@ const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 
 const contract = require("../artifacts/contracts/HelloWorld.sol/HelloWorld.json");
 console.log(JSON.stringify(contract.abi));
+
+//Provider
+const alchemyProvider = new ethers.providers.AlchemyProvider(
+	(network = "ropsten"),
+	API_KEY
+);
+
+//Signer
+const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
+
+//Contract
+const helloWorldContract = new ethers.Contract(
+	CONTRACT_ADDRESS,
+	contract.abi,
+	signer
+)
+
+
+async function main() {
+	const message = await helloWorldContract.message();
+	console.log(message);
+}
+
+main()
